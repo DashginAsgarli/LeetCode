1965043edc72edb4b392873fb799e8d543c5db35
DashginAsgarli <dashqinasgarli@gmail.com>
2026-08-12 01:27:02 +0400

    Add solutions for LeetCode and SQL problems

--- sql/easy/0182.-duplicate-emails.sql
+++ sql/easy/0182.-duplicate-emails.sql
@@ -1,4 +1,5 @@
--- Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+-- Write a solution to report all the duplicate emails. 
+-- Note that it's guaranteed that the email field is not NULL.
 
 -- Return the result table in any order.
 
--- sql/hard/0178-rank-scores.sql
+++ sql/hard/0178-rank-scores.sql
@@ -2,7 +2,8 @@
 
 -- The scores should be ranked from the highest to the lowest.
 -- If there is a tie between two scores, both should have the same ranking.
--- After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+-- After a tie, the next ranking number should be the next consecutive integer value. 
+-- In other words, there should be no holes between ranks.
 -- Return the result table ordered by score in descending order.
 
 
